77495e912190bae7b5056bf392819da84d78940e
DominicOrga <dominicorga@gmail.com>
2016-11-25 16:51:37 +0800

    First commit

---
+++ Source/ProjectGates/GatesClassic.cpp
@@ -0,0 +1,28 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectGates.h"
+#include "GatesClassic.h"
+
+
+// Sets default values
+AGatesClassic::AGatesClassic()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AGatesClassic::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AGatesClassic::Tick( float DeltaTime )
+{
+	Super::Tick( DeltaTime );
+
+}
+
---
+++ Source/ProjectGates/ProjectGatesGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "ProjectGatesGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTGATES_API AProjectGatesGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
